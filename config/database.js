da0a564c987483ed5000dc8cb2a212713cff2efa
quoccuong2005 <161569929+quoccuong2005@users.noreply.github.com>
2024-12-25 10:18:51 +0700

    .

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "tuyendung",
+  host: "bqj7wk0anaf5zxuig8ke-mysql.services.clever-cloud.com",
+  port: 3306,
+  user: "us3ngpct7bxwm7rb",
+  password: "Kz1sxmSvga6XbEUFIXwk",
+  database: "bqj7wk0anaf5zxuig8ke",
 });
-
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to the database:", err);
